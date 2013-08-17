0a186bc1e5f5d3682acb8a5b6afd03b7222219dc
codistmonk <codistmonk@users.sourceforge.net>
2013-08-17 10:46:28 +0200

    Added building pad model to city demo

--- demos/city/city.py
+++ demos/city/city.py
@@ -1,5 +1,3 @@
-from math import sqrt
-
 from direct.showbase.ShowBase import ShowBase
 from direct.directnotify.DirectNotify import DirectNotify
 from panda3d.core import Mat4
@@ -14,15 +12,15 @@ class MyApp(ShowBase):
 		self.loadModels()
 		self.setCameraPos(0, 0, 2)
 
-
 	def loadModels(self):
 		self.loadSky()
 		self.loadTerrain()
+		self.loadBuildings()
 
 	def loadSky(self):
 		self.sky = self.loader.loadModel("models/sky")
 		self.sky.reparentTo(self.camera)
-		self.sky.setScale(base.camLens.getNear() * sqrt(2))
+		self.sky.setScale(base.camLens.getNear() * 1.1)
 		self.sky.setBin("background", 0)
 		self.sky.setDepthWrite(False)
 		self.sky.setCompass()
@@ -30,6 +28,11 @@ class MyApp(ShowBase):
 	def loadTerrain(self):
 		self.terrain = self.loader.loadModel("models/terrain")
 		self.terrain.reparentTo(self.render)
+
+	def loadBuildings(self):
+		buildingPadPrototype = self.loader.loadModel("models/building_pad")
+		buildingPadInstance = render.attachNewNode("Building-Pad-Instance")
+		buildingPadPrototype.instanceTo(buildingPadInstance)
 
 	def setCameraPos(self, x, y, z):
 		base.disableMouse()
