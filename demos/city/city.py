f7ae069283dcdba1b9bf56a840257efd8c7b0675
codistmonk <codistmonk@users.sourceforge.net>
2013-08-17 04:18:10 +0200

    Added simple terrain to city demo

--- demos/city/city.py
+++ demos/city/city.py
@@ -1,5 +1,6 @@
 from direct.showbase.ShowBase import ShowBase
 from direct.directnotify.DirectNotify import DirectNotify
+from panda3d.core import Mat4
 
 class MyApp(ShowBase):
 
@@ -9,14 +10,33 @@ class MyApp(ShowBase):
 		self.debug = DirectNotify().newCategory("Debug")
 
 		self.loadModels()
+		self.setCameraPos(0, 0, 2)
+
 
 	def loadModels(self):
+		self.loadSky()
+		self.loadTerrain()
+
+	def loadSky(self):
 		self.sky = self.loader.loadModel("models/sky")
-		self.sky.reparentTo(self.cam)
+		self.sky.reparentTo(self.camera)
 		self.sky.setScale(2, 2, 2)
 		self.sky.setBin("background", 0)
 		self.sky.setDepthWrite(False)
 		self.sky.setCompass()
+
+	def loadTerrain(self):
+		self.terrain = self.loader.loadModel("models/terrain")
+		self.terrain.reparentTo(self.render)
+		self.terrain.setScale(1000, 1000, 1)
+
+	def setCameraPos(self, x, y, z):
+		base.disableMouse()
+		self.camera.setPos(x, y, z)
+		mat = Mat4(self.camera.getMat())
+		mat.invertInPlace()
+		base.mouseInterfaceNode.setMat(mat)
+		base.enableMouse()
 
 app = MyApp()
 app.run()
