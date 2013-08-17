c4f6ca833e505d7060889fac89e028e7c032256c
codistmonk <codistmonk@users.sourceforge.net>
2013-08-17 09:57:31 +0200

    Refactoring

--- demos/city/city.py
+++ demos/city/city.py
@@ -1,3 +1,5 @@
+from math import sqrt
+
 from direct.showbase.ShowBase import ShowBase
 from direct.directnotify.DirectNotify import DirectNotify
 from panda3d.core import Mat4
@@ -20,7 +22,7 @@ class MyApp(ShowBase):
 	def loadSky(self):
 		self.sky = self.loader.loadModel("models/sky")
 		self.sky.reparentTo(self.camera)
-		self.sky.setScale(2, 2, 2)
+		self.sky.setScale(base.camLens.getNear() * sqrt(2))
 		self.sky.setBin("background", 0)
 		self.sky.setDepthWrite(False)
 		self.sky.setCompass()
@@ -28,7 +30,6 @@ class MyApp(ShowBase):
 	def loadTerrain(self):
 		self.terrain = self.loader.loadModel("models/terrain")
 		self.terrain.reparentTo(self.render)
-		self.terrain.setScale(1000, 1000, 1)
 
 	def setCameraPos(self, x, y, z):
 		base.disableMouse()
