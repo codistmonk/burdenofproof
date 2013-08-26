65bdc544b2b45c56334c2375ca7265e6905e6136
codistmonk <codistmonk@users.sourceforge.net>
2013-08-26 21:34:32 +0200

    C++/Python integration in demo1

--- demos/demo1/Application.py
+++ demos/demo1/Application.py
@@ -1,11 +1,14 @@
 from panda3d.core import *
 from direct.showbase.ShowBase import ShowBase
+from bop import *
 
-class MyApp(ShowBase):
+class Application(ShowBase):
 
 	def __init__(self):
 		ShowBase.__init__(self)
 
+		self.game = Game()
+
 loadPrcFile("myconfig.prc")
 
-MyApp().run()
+Application().run()
