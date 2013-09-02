87963f9fb6f19084e3c40b60d640d240877492e6
codistmonk <codistmonk@users.sourceforge.net>
2013-09-02 14:49:40 +0200

    Updated city eggs in demo1

--- demos/demo1/generatecity.py
+++ demos/demo1/generatecity.py
@@ -22,6 +22,17 @@ def isRoad(blueprintItem):
 def isGround(blueprintItem):
 	return not isBuilding(blueprintItem) and not isRoad(blueprintItem)
 
+def getItem(blueprint, nsIndex, weIndex):
+	if nsIndex < 0 or len(blueprint) <= nsIndex:
+		return "_"
+
+	blueprintRow = blueprint[nsIndex]
+
+	if weIndex < 0 or len(blueprintRow) <= weIndex:
+		return "_"
+
+	return blueprintRow[weIndex]
+
 blueprintPath = "models/cityblueprint.txt"
 
 blueprint = [line.strip() for line in open(blueprintPath)]
@@ -51,5 +62,32 @@ for nsIndex, blueprintRow in enumerate(blueprint):
 			block.addChild(EggExternalReference("road", "road"))
 		else:
 			block.addChild(EggExternalReference("ground", "ground"))
+
+		if not isBuilding(blueprintItem):
+			nwItem = getItem(blueprint, nsIndex - 1, weIndex - 1)
+			nItem = getItem(blueprint, nsIndex - 1, weIndex)
+			neItem = getItem(blueprint, nsIndex - 1, weIndex + 1)
+			wItem = getItem(blueprint, nsIndex, weIndex - 1)
+			eItem = getItem(blueprint, nsIndex, weIndex + 1)
+			swItem = getItem(blueprint, nsIndex + 1, weIndex - 1)
+			sItem = getItem(blueprint, nsIndex + 1, weIndex)
+			seItem = getItem(blueprint, nsIndex + 1, weIndex + 1)
+
+			if wItem != blueprintItem and nItem != blueprintItem:
+				block.addChild(EggExternalReference("sidewalk", "nwexteriorsidewalk"))
+			if eItem != blueprintItem and nItem != blueprintItem:
+				block.addChild(EggExternalReference("sidewalk", "neexteriorsidewalk"))
+			if wItem != blueprintItem and sItem != blueprintItem:
+				block.addChild(EggExternalReference("sidewalk", "swexteriorsidewalk"))
+			if eItem != blueprintItem and sItem != blueprintItem:
+				block.addChild(EggExternalReference("sidewalk", "seexteriorsidewalk"))
+			if wItem == blueprintItem and nItem == blueprintItem and nwItem != blueprintItem:
+				block.addChild(EggExternalReference("sidewalk", "nwinteriorsidewalk"))
+			if eItem == blueprintItem and nItem == blueprintItem and neItem != blueprintItem:
+				block.addChild(EggExternalReference("sidewalk", "neinteriorsidewalk"))
+			if wItem == blueprintItem and sItem == blueprintItem and swItem != blueprintItem:
+				block.addChild(EggExternalReference("sidewalk", "swinteriorsidewalk"))
+			if eItem == blueprintItem and sItem == blueprintItem and seItem != blueprintItem:
+				block.addChild(EggExternalReference("sidewalk", "seinteriorsidewalk"))
 
 city.writeEgg("models/city.egg")
