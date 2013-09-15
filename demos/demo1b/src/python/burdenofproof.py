dbd9e501665e4487b14f01f56c87b903d59031a7
codistmonk <codistmonk@users.sourceforge.net>
2013-09-15 11:40:39 +0200

    Added application startup code in demo1b

--- demos/demo1b/src/python/burdenofproof.py
+++ demos/demo1b/src/python/burdenofproof.py
@@ -1,4 +1,24 @@
+from direct.showbase.ShowBase import ShowBase
 from bop import *
 from bopmodel import *
+from utils import *
 
 Hello()
+
+class BurdenOfProof(ShowBase):
+
+    def __init__(self):
+        ShowBase.__init__(self)
+
+        self.useAdvancedVisualEffects = ConfigVariableBool("use-advanced-visual-effects", True)
+
+        self.game = Game()
+
+        self.setupKeyboardControl()
+
+    def setupKeyboardControl(self):
+        self.accept("escape", sys.exit)
+
+loadPrcFile("myconfig.prc")
+
+BurdenOfProof().run()
--- demos/demo1b/src/python/utils.py
+++ demos/demo1b/src/python/utils.py
@@ -3,41 +3,58 @@ from panda3d.core import *
 from panda3d.egg import *
 
 def clamp(value, minValue, maxValue):
-	return min(max(minValue, value), maxValue)
+    return min(max(minValue, value), maxValue)
 
 def clampX(target, minX, maxX):
-	target.setX(clamp(target.getX(), minX, maxX))
+    target.setX(clamp(target.getX(), minX, maxX))
 
 def clampY(target, minY, maxY):
-	target.setY(clamp(target.getY(), minY, maxY))
+    target.setY(clamp(target.getY(), minY, maxY))
 
 def clampZ(target, minZ, maxZ):
-	target.setZ(clamp(target.getZ(), minZ, maxZ))
+    target.setZ(clamp(target.getZ(), minZ, maxZ))
 
 def addEggVertex(eggVertexPool, x, y, z, u = 0, v = 0, useUv = True):
-	vertex = EggVertex()
+    vertex = EggVertex()
 
-	vertex.setPos(Point3D(x, y, z))
+    vertex.setPos(Point3D(x, y, z))
 
-	if useUv:
-		vertex.setUv(Point2D(u, v))
+    if useUv:
+        vertex.setUv(Point2D(u, v))
 
-	eggVertexPool.addVertex(vertex)
+    eggVertexPool.addVertex(vertex)
 
-	return vertex
+    return vertex
 
 def newGroup(eggData):
-	group = EggGroup()
+    group = EggGroup()
 
-	group.setGroupType(EggGroup.GTInstance)
+    group.setGroupType(EggGroup.GTInstance)
 
-	eggData.addChild(group)
+    eggData.addChild(group)
 
-	return group
+    return group
 
 def finishEgg(eggData):
-	eggData.recomputeVertexNormals(0.0)
-	eggData.recomputeTangentBinormalAuto()
-	eggData.removeUnusedVertices(True)
+    eggData.recomputeVertexNormals(0.0)
+    eggData.recomputeTangentBinormalAuto()
+    eggData.removeUnusedVertices(True)
 
+applicationName = "burdenofproof"
 scriptPath = os.path.dirname(sys.argv[0]).replace("\\", "/")
+
+if sys.platform == 'darwin':
+    from AppKit import NSSearchPathForDirectoriesInDomains
+    # http://developer.apple.com/DOCUMENTATION/Cocoa/Reference/Foundation/Miscellaneous/Foundation_Functions/Reference/reference.html#//apple_ref/c/func/NSSearchPathForDirectoriesInDomains
+    # NSApplicationSupportDirectory = 14
+    # NSUserDomainMask = 1
+    # True for expanding the tilde into a fully qualified path
+    applicationDataPath = os.path.join(NSSearchPathForDirectoriesInDomains(14, 1, True)[0], APPNAME)
+elif sys.platform == 'win32':
+    applicationDataPath = os.path.join(os.environ['APPDATA'], applicationName)
+else:
+    applicationDataPath = os.path.expanduser(os.path.join("~", "." + applicationName))
+
+print "applicationName:", applicationName
+print "scriptPath:", scriptPath
+print "applicationDataPath:", applicationDataPath
