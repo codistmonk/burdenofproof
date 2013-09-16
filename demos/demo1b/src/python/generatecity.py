caa3e990fc1bdbb901dd3bc27e02c9d8b9dc5330
codistmonk <codistmonk@users.sourceforge.net>
2013-09-16 08:14:51 +0200

    Added pep8 Python checker to demo1b

--- demos/demo1b/src/python/generatecity.py
+++ demos/demo1b/src/python/generatecity.py
@@ -5,30 +5,30 @@ from panda3d.egg import *
 from utils import *
 
 def isBuilding(blueprintItem):
-	return blueprintItem == 'P' or blueprintItem == 'O' or blueprintItem == 'H'
+    return blueprintItem == 'P' or blueprintItem == 'O' or blueprintItem == 'H'
 
 def isRoad(blueprintItem):
-	return blueprintItem == 'S'
+    return blueprintItem == 'S'
 
 def isGround(blueprintItem):
-	return not isBuilding(blueprintItem) and not isRoad(blueprintItem)
+    return not isBuilding(blueprintItem) and not isRoad(blueprintItem)
 
 def getItem(blueprint, nsIndex, weIndex):
-	if nsIndex < 0 or len(blueprint) <= nsIndex:
-		return "_"
+    if nsIndex < 0 or len(blueprint) <= nsIndex:
+        return "_"
 
-	blueprintRow = blueprint[nsIndex]
+    blueprintRow = blueprint[nsIndex]
 
-	if weIndex < 0 or len(blueprintRow) <= weIndex:
-		return "_"
+    if weIndex < 0 or len(blueprintRow) <= weIndex:
+        return "_"
 
-	return blueprintRow[weIndex]
+    return blueprintRow[weIndex]
 
 def addExternalReference(nodeName, fileName, block, outlineBlock = None):
-	block.addChild(EggExternalReference(nodeName, fileName))
+    block.addChild(EggExternalReference(nodeName, fileName))
 
-	if not outlineBlock is None:
-		outlineBlock.addChild(EggExternalReference(nodeName, fileName + "_outline"))
+    if not outlineBlock is None:
+        outlineBlock.addChild(EggExternalReference(nodeName, fileName + "_outline"))
 
 blueprintPath = "models/cityblueprint.txt"
 
@@ -46,81 +46,81 @@ city = EggData()
 cityOuline = EggData()
 
 for nsIndex, blueprintRow in enumerate(blueprint):
-	blockZ = -(citySizeNS / 2 - (nsIndex + 1) * blockSize)
+    blockZ = -(citySizeNS / 2 - (nsIndex + 1) * blockSize)
 
-	for weIndex, blueprintItem in enumerate(blueprintRow):
-		blockX = weIndex * blockSize - citySizeWE / 2
-		block = newGroup(city)
-		outlineBlock = newGroup(cityOuline)
-		block.addTranslate3d(Vec3D(blockX, 0.0, blockZ))
-		outlineBlock.addTranslate3d(Vec3D(blockX, 0.0, blockZ))
+    for weIndex, blueprintItem in enumerate(blueprintRow):
+        blockX = weIndex * blockSize - citySizeWE / 2
+        block = newGroup(city)
+        outlineBlock = newGroup(cityOuline)
+        block.addTranslate3d(Vec3D(blockX, 0.0, blockZ))
+        outlineBlock.addTranslate3d(Vec3D(blockX, 0.0, blockZ))
 
-		if isBuilding(blueprintItem):
-			addExternalReference("building", "building", block)
-			addExternalReference("buildingPad", "buildingpad", block, outlineBlock)
-		elif isRoad(blueprintItem):
-			addExternalReference("road", "road", block)
-		else:
-			addExternalReference("ground", "ground", block)
+        if isBuilding(blueprintItem):
+            addExternalReference("building", "building", block)
+            addExternalReference("buildingPad", "buildingpad", block, outlineBlock)
+        elif isRoad(blueprintItem):
+            addExternalReference("road", "road", block)
+        else:
+            addExternalReference("ground", "ground", block)
 
-		if not isBuilding(blueprintItem):
-			nwItem = getItem(blueprint, nsIndex - 1, weIndex - 1)
-			nItem = getItem(blueprint, nsIndex - 1, weIndex)
-			neItem = getItem(blueprint, nsIndex - 1, weIndex + 1)
-			wItem = getItem(blueprint, nsIndex, weIndex - 1)
-			eItem = getItem(blueprint, nsIndex, weIndex + 1)
-			swItem = getItem(blueprint, nsIndex + 1, weIndex - 1)
-			sItem = getItem(blueprint, nsIndex + 1, weIndex)
-			seItem = getItem(blueprint, nsIndex + 1, weIndex + 1)
+        if not isBuilding(blueprintItem):
+            nwItem = getItem(blueprint, nsIndex - 1, weIndex - 1)
+            nItem = getItem(blueprint, nsIndex - 1, weIndex)
+            neItem = getItem(blueprint, nsIndex - 1, weIndex + 1)
+            wItem = getItem(blueprint, nsIndex, weIndex - 1)
+            eItem = getItem(blueprint, nsIndex, weIndex + 1)
+            swItem = getItem(blueprint, nsIndex + 1, weIndex - 1)
+            sItem = getItem(blueprint, nsIndex + 1, weIndex)
+            seItem = getItem(blueprint, nsIndex + 1, weIndex + 1)
 
-			if isRoad(nItem) and isRoad(blueprintItem) and isRoad(eItem) and not isRoad(wItem) and not isRoad(sItem):
-				addExternalReference("marking", "swmarking", block)
-			if isRoad(wItem) and isRoad(blueprintItem) and isRoad(nItem) and not isRoad(sItem) and not isRoad(eItem):
-				addExternalReference("marking", "semarking", block)
-			if isRoad(sItem) and isRoad(blueprintItem) and isRoad(wItem) and not isRoad(eItem) and not isRoad(nItem):
-				addExternalReference("marking", "nemarking", block)
-			if isRoad(eItem) and isRoad(blueprintItem) and isRoad(sItem) and not isRoad(nItem) and not isRoad(wItem):
-				addExternalReference("marking", "nwmarking", block)
+            if isRoad(nItem) and isRoad(blueprintItem) and isRoad(eItem) and not isRoad(wItem) and not isRoad(sItem):
+                addExternalReference("marking", "swmarking", block)
+            if isRoad(wItem) and isRoad(blueprintItem) and isRoad(nItem) and not isRoad(sItem) and not isRoad(eItem):
+                addExternalReference("marking", "semarking", block)
+            if isRoad(sItem) and isRoad(blueprintItem) and isRoad(wItem) and not isRoad(eItem) and not isRoad(nItem):
+                addExternalReference("marking", "nemarking", block)
+            if isRoad(eItem) and isRoad(blueprintItem) and isRoad(sItem) and not isRoad(nItem) and not isRoad(wItem):
+                addExternalReference("marking", "nwmarking", block)
 
-			if isRoad(wItem) and isRoad(blueprintItem) and isRoad(eItem) and (not isRoad(nItem) or not isRoad(sItem)):
-				addExternalReference("marking", "wemarking", block)
-			if isRoad(nItem) and isRoad(blueprintItem) and isRoad(sItem) and (not isRoad(wItem) or not isRoad(eItem)):
-				addExternalReference("marking", "nsmarking", block)
+            if isRoad(wItem) and isRoad(blueprintItem) and isRoad(eItem) and (not isRoad(nItem) or not isRoad(sItem)):
+                addExternalReference("marking", "wemarking", block)
+            if isRoad(nItem) and isRoad(blueprintItem) and isRoad(sItem) and (not isRoad(wItem) or not isRoad(eItem)):
+                addExternalReference("marking", "nsmarking", block)
 
-			if wItem != blueprintItem and nItem != blueprintItem:
-				addExternalReference("sidewalk", "nwexteriorsidewalk", block, outlineBlock)
-			if eItem != blueprintItem and nItem != blueprintItem:
-				addExternalReference("sidewalk", "neexteriorsidewalk", block, outlineBlock)
-			if wItem != blueprintItem and sItem != blueprintItem:
-				addExternalReference("sidewalk", "swexteriorsidewalk", block, outlineBlock)
-			if eItem != blueprintItem and sItem != blueprintItem:
-				addExternalReference("sidewalk", "seexteriorsidewalk", block, outlineBlock)
+            if wItem != blueprintItem and nItem != blueprintItem:
+                addExternalReference("sidewalk", "nwexteriorsidewalk", block, outlineBlock)
+            if eItem != blueprintItem and nItem != blueprintItem:
+                addExternalReference("sidewalk", "neexteriorsidewalk", block, outlineBlock)
+            if wItem != blueprintItem and sItem != blueprintItem:
+                addExternalReference("sidewalk", "swexteriorsidewalk", block, outlineBlock)
+            if eItem != blueprintItem and sItem != blueprintItem:
+                addExternalReference("sidewalk", "seexteriorsidewalk", block, outlineBlock)
 
-			if wItem == blueprintItem and nItem == blueprintItem and nwItem != blueprintItem:
-				addExternalReference("sidewalk", "nwinteriorsidewalk", block, outlineBlock)
-			if eItem == blueprintItem and nItem == blueprintItem and neItem != blueprintItem:
-				addExternalReference("sidewalk", "neinteriorsidewalk", block, outlineBlock)
-			if wItem == blueprintItem and sItem == blueprintItem and swItem != blueprintItem:
-				addExternalReference("sidewalk", "swinteriorsidewalk", block, outlineBlock)
-			if eItem == blueprintItem and sItem == blueprintItem and seItem != blueprintItem:
-				addExternalReference("sidewalk", "seinteriorsidewalk", block, outlineBlock)
+            if wItem == blueprintItem and nItem == blueprintItem and nwItem != blueprintItem:
+                addExternalReference("sidewalk", "nwinteriorsidewalk", block, outlineBlock)
+            if eItem == blueprintItem and nItem == blueprintItem and neItem != blueprintItem:
+                addExternalReference("sidewalk", "neinteriorsidewalk", block, outlineBlock)
+            if wItem == blueprintItem and sItem == blueprintItem and swItem != blueprintItem:
+                addExternalReference("sidewalk", "swinteriorsidewalk", block, outlineBlock)
+            if eItem == blueprintItem and sItem == blueprintItem and seItem != blueprintItem:
+                addExternalReference("sidewalk", "seinteriorsidewalk", block, outlineBlock)
 
-			if wItem != blueprintItem and nItem == blueprintItem:
-				addExternalReference("sidewalk", "nwhalf1sidewalk", block, outlineBlock)
-			if eItem != blueprintItem and nItem == blueprintItem:
-				addExternalReference("sidewalk", "nehalf2sidewalk", block, outlineBlock)
-			if wItem != blueprintItem and sItem == blueprintItem:
-				addExternalReference("sidewalk", "swhalf2sidewalk", block, outlineBlock)
-			if eItem != blueprintItem and sItem == blueprintItem:
-				addExternalReference("sidewalk", "sehalf1sidewalk", block, outlineBlock)
-			if wItem == blueprintItem and nItem != blueprintItem:
-				addExternalReference("sidewalk", "nwhalf2sidewalk", block, outlineBlock)
-			if eItem == blueprintItem and nItem != blueprintItem:
-				addExternalReference("sidewalk", "nehalf1sidewalk", block, outlineBlock)
-			if wItem == blueprintItem and sItem != blueprintItem:
-				addExternalReference("sidewalk", "swhalf1sidewalk", block, outlineBlock)
-			if eItem == blueprintItem and sItem != blueprintItem:
-				addExternalReference("sidewalk", "sehalf2sidewalk", block, outlineBlock)
+            if wItem != blueprintItem and nItem == blueprintItem:
+                addExternalReference("sidewalk", "nwhalf1sidewalk", block, outlineBlock)
+            if eItem != blueprintItem and nItem == blueprintItem:
+                addExternalReference("sidewalk", "nehalf2sidewalk", block, outlineBlock)
+            if wItem != blueprintItem and sItem == blueprintItem:
+                addExternalReference("sidewalk", "swhalf2sidewalk", block, outlineBlock)
+            if eItem != blueprintItem and sItem == blueprintItem:
+                addExternalReference("sidewalk", "sehalf1sidewalk", block, outlineBlock)
+            if wItem == blueprintItem and nItem != blueprintItem:
+                addExternalReference("sidewalk", "nwhalf2sidewalk", block, outlineBlock)
+            if eItem == blueprintItem and nItem != blueprintItem:
+                addExternalReference("sidewalk", "nehalf1sidewalk", block, outlineBlock)
+            if wItem == blueprintItem and sItem != blueprintItem:
+                addExternalReference("sidewalk", "swhalf1sidewalk", block, outlineBlock)
+            if eItem == blueprintItem and sItem != blueprintItem:
+                addExternalReference("sidewalk", "sehalf2sidewalk", block, outlineBlock)
 
 city.writeEgg("models/city.egg")
 cityOuline.writeEgg("models/city_outline.egg")
--- demos/demo1b/src/python/orbitalcameracontroller.py
+++ demos/demo1b/src/python/orbitalcameracontroller.py
@@ -5,70 +5,70 @@ from utils import *
 
 class OrbitalCameraController:
 
-	def __init__(self, scene):
-		self.scene = scene
-		self.mouseButton = [0, 0, 0]
-		self.target = Vec3(0.0, 0.0, 0.0)
-		self.distanceFromTarget = 64.0
-		self.last = 0
-		self.mouseVisible = True
-		self.mouseLastVisibleLocation = None
+    def __init__(self, scene):
+        self.scene = scene
+        self.mouseButton = [0, 0, 0]
+        self.target = Vec3(0.0, 0.0, 0.0)
+        self.distanceFromTarget = 64.0
+        self.last = 0
+        self.mouseVisible = True
+        self.mouseLastVisibleLocation = None
 
-		scene.disableMouse()
+        scene.disableMouse()
 
-		scene.accept("mouse1", self.setMouseButton, [0, 1])
-		scene.accept("mouse1-up", self.setMouseButton, [0, 0])
-		scene.accept("wheel_up", self.scaleDistanceFromTarget, [0.75])
-		scene.accept("wheel_down", self.scaleDistanceFromTarget, [1.25])
+        scene.accept("mouse1", self.setMouseButton, [0, 1])
+        scene.accept("mouse1-up", self.setMouseButton, [0, 0])
+        scene.accept("wheel_up", self.scaleDistanceFromTarget, [0.75])
+        scene.accept("wheel_down", self.scaleDistanceFromTarget, [1.25])
 
-		scene.taskMgr.add(self.controlCamera, "cameraTask")
+        scene.taskMgr.add(self.controlCamera, "cameraTask")
 
-	def setMouseButton(self, button, value):
-		self.mouseButton[button] = value
+    def setMouseButton(self, button, value):
+        self.mouseButton[button] = value
 
-		if button == 0:
-			self.setMouseVisible(value == 0)
+        if button == 0:
+            self.setMouseVisible(value == 0)
 
-	def scaleDistanceFromTarget(self, scale):
-		self.distanceFromTarget *= scale
+    def scaleDistanceFromTarget(self, scale):
+        self.distanceFromTarget *= scale
 
-	def controlCamera(self, task):
-		if self.mouseButton[0]:
-			mouse = self.scene.win.getPointer(0)
-			x = mouse.getX()
-			y = mouse.getY()
-			mousePreviousX = self.mouseLastVisibleLocation.getX()
-			mousePreviousY = self.mouseLastVisibleLocation.getY()
-			heading = self.scene.camera.getH()
-			pitch = self.scene.camera.getP()
+    def controlCamera(self, task):
+        if self.mouseButton[0]:
+            mouse = self.scene.win.getPointer(0)
+            x = mouse.getX()
+            y = mouse.getY()
+            mousePreviousX = self.mouseLastVisibleLocation.getX()
+            mousePreviousY = self.mouseLastVisibleLocation.getY()
+            heading = self.scene.camera.getH()
+            pitch = self.scene.camera.getP()
 
-			if self.scene.win.movePointer(0, mousePreviousX, mousePreviousY):
-				heading -= (x - mousePreviousX) * 0.2
-				pitch = clamp(pitch - (y - mousePreviousY) * 0.2, -89, 89)
-				self.scene.camera.setHpr(heading, pitch, 0)
+            if self.scene.win.movePointer(0, mousePreviousX, mousePreviousY):
+                heading -= (x - mousePreviousX) * 0.2
+                pitch = clamp(pitch - (y - mousePreviousY) * 0.2, -89, 89)
+                self.scene.camera.setHpr(heading, pitch, 0)
 
-		direction = self.scene.camera.getMat().getRow3(1)
-		self.scene.camera.setPos(self.target - direction * self.distanceFromTarget)
+        direction = self.scene.camera.getMat().getRow3(1)
+        self.scene.camera.setPos(self.target - direction * self.distanceFromTarget)
 
-		return Task.cont
+        return Task.cont
 
-	def setMouseVisible(self, mouseVisible):
-		if (self.mouseVisible != mouseVisible):
-			self.mouseVisible = mouseVisible
+    def setMouseVisible(self, mouseVisible):
+        if (self.mouseVisible != mouseVisible):
+            self.mouseVisible = mouseVisible
 
-			props = WindowProperties()
-			props.setCursorHidden(not mouseVisible)
-			self.scene.win.requestProperties(props)
+            props = WindowProperties()
+            props.setCursorHidden(not mouseVisible)
+            self.scene.win.requestProperties(props)
 
-			if (not mouseVisible):
-				self.mouseLastVisibleLocation = self.scene.win.getPointer(0)
+            if (not mouseVisible):
+                self.mouseLastVisibleLocation = self.scene.win.getPointer(0)
 
-	def tictoc(self, task):
-		elapsed = task.time - self.last
+    def tictoc(self, task):
+        elapsed = task.time - self.last
 
-		if (self.last == 0):
-			elapsed = 0
+        if (self.last == 0):
+            elapsed = 0
 
-		self.last = task.time
+        self.last = task.time
 
-		return elapsed
+        return elapsed
